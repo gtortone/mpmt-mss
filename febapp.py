0ea7ecdd2b3728920bfd5348f3483ae2f1ea0bd9
Gennaro Tortone <gennaro.tortone@na.infn.it>
2026-06-17 14:52:21 +0000

    refactor febapp.py

--- febapp.py
+++ febapp.py
@@ -1,8 +1,7 @@
 
 from mini_rpc import RPCRuntime, create_app
 from runcontrol.fpga import FPGA
-from sensors.housekeeping import HouseKeeping
-from feb import ModbusManager, ModbusConfig, FEBManager, DeviceChannel, DeviceType, PMTChannel, LEDChannel
+from feb import FEBManager, ModbusConfig
 
 febmgr = FEBManager(ModbusConfig(mode="rtu", port="/dev/ttyPS1"))
 
@@ -16,3 +15,4 @@ runtime.register_service("fpga.core", fpga)
 runtime.register_service("febmgr", febmgr)
 
 app = create_app(runtime)
+
