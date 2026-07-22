47b8ed663f30ffb7b59af94904571b615b6fe089
Gennaro Tortone <gtortone@gmail.com>
2026-07-22 15:41:02 +0200

    change prefix fpga.core to fpga

--- src/mpmt_mss/main.py
+++ src/mpmt_mss/main.py
@@ -11,7 +11,7 @@ fpga = FPGA('/dev/uio0')
 runtime = RPCRuntime()
 
 # core layer
-runtime.register_service("fpga.core", fpga)
+runtime.register_service("fpga", fpga)
 runtime.register_service("febmgr", febmgr)
 
 app = create_app(runtime)
